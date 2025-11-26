8e0e7512e1166b3fe14579dde92309fa07391f44
jimlecracken14 <jimlecracken13@gmail.com>
2025-11-26 17:02:27 +0000

    commit

--- lib/pages/jeuner/mentor_actif_detail_page.dart
+++ lib/pages/jeuner/mentor_actif_detail_page.dart
@@ -415,9 +415,9 @@ class _MentorActifDetailPageState extends State<MentorActifDetailPage> {
     return Container(
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: color.withOpacity(0.1),
+        color: color.withValues(alpha:0.1),
         borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: color.withOpacity(0.3)),
+        border: Border.all(color: color.withValues(alpha:0.3)),
       ),
       child: Column(
         children: [
